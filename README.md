717efcacb84086c2aa08dd1ffddbd695f3054672
HASH <h8shim@gmail.com>
2026-05-04 04:50:14 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Just put the diffusion model in this dir and run the script
+
+No dependencies or venvs or anything else needed
+
+I used this tool: https://github.com/leejet/stable-diffusion.cpp
---
+++ diffusion_to_gguf.py
@@ -0,0 +1,73 @@
+import os, subprocess, sys
+
+sdcpp = "sdcpp/sd-cli"
+
+inputs = []
+for entry in os.scandir("."):
+    if entry.name.endswith(".safetensors"):
+        inputs.append(entry.name)
+
+TYPES = [
+    "f16",
+    "f32",
+    "q8_0",
+    "q5_0",
+    "q5_1", 
+    "q4_0", 
+    "q4_1",
+    "q4_K",
+    "q3_K",
+    "q2_K"
+]
+
+if __name__ == '__main__':
+    os.makedirs("output", exist_ok=True)
+
+    while not os.path.isdir(os.path.dirname(sdcpp)):
+        if os.path.isdir("bin"):
+            sdcpp = "bin/sd-cli"
+            continue
+        print("\nPlease install stable-diffusion.cpp in current dir under 'sdcpp/' or 'bin/'.")
+        exit()
+
+    if sys.platform.startswith("linux"):
+        sdcpp = sdcpp + "l"
+        #subprocess.Popen(["chmod", "700", sdcpp], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='.', text=True)
+
+    while True:
+
+        available = ""
+        for i, m in enumerate(inputs):
+            available += f"{i}. {m} \n"
+        try: r = int(input(f"\nChoose the INPUT Model: (0 = Defualt) \n\n{available} \n\n>> "))
+        except: r = 0
+        inputModel = inputs[r]
+
+        available = ""
+        for i, m in enumerate(TYPES):
+            available += f"{i}. {m} \n"
+        try: r = int(input(f"\nChoose the QUANT: (0 = Defualt) \n\n{available} \n\n>> "))
+        except: r = 0
+        outtype = TYPES[r]
+        
+        cmd = [
+            sdcpp, 
+            '-M', 'convert',
+            '-m', inputModel,
+            "-o", f"output/{inputModel}".replace(".safetensors", f"-{outtype}.gguf"),
+            "-v",
+            "--type", outtype
+        ]
+
+        print("\n" + " ".join(cmd))
+
+        if input("\nWould you like to run the command as a subprocess? (Y/n) \n\n>> ").lower() == 'n':
+            continue
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd='.', text=True)
+
+        for line in process.stdout:
+            print(line, end='') 
+
+        process.wait()
+
+        input("\n\n-- Process Ended --")
